43693d343693b49946674f44850806b5d954891b
Oddjob922 <tyler.922@hotmail.com>
2016-01-22 22:16:16 -0500

    Forgot to commit this

--- gaia/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FtcRobotControllerActivity.java
+++ gaia/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FtcRobotControllerActivity.java
@@ -62,7 +62,6 @@ import com.qualcomm.ftccommon.FtcRobotControllerService.FtcRobotControllerBinder
 import com.qualcomm.ftccommon.LaunchActivityConstantsList;
 import com.qualcomm.ftccommon.Restarter;
 import com.qualcomm.ftccommon.UpdateUI;
-import com.qualcomm.ftcrobotcontroller.opmodes.CameraOp;
 import com.qualcomm.ftcrobotcontroller.opmodes.FtcOpModeRegister;
 import com.qualcomm.hardware.HardwareFactory;
 import com.qualcomm.robotcore.hardware.configuration.Utility;
@@ -410,14 +409,14 @@ public class FtcRobotControllerActivity extends Activity {
     return cam;
   }
 
-  public void initPreview(final Camera camera, final CameraOp context, final Camera.PreviewCallback previewCallback) {
-    runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        context.preview = new CameraPreview(FtcRobotControllerActivity.this, camera, previewCallback);
-        FrameLayout previewLayout = (FrameLayout) findViewById(R.id.previewLayout);
-        previewLayout.addView(context.preview);
-      }
-    });
+  //public void initPreview(final Camera camera, final context, final Camera.PreviewCallback previewCallback) {
+    //runOnUiThread(new Runnable() {
+      //@Override
+      //public void run() {
+        //context.preview = new CameraPreview(FtcRobotControllerActivity.this, camera, previewCallback);
+        //FrameLayout previewLayout = (FrameLayout) findViewById(R.id.previewLayout);
+        //previewLayout.addView(context.preview);
+      //}
+    //});
   }
-}
+//}
