96c322a5b940a3b13c6509f02d97386dc90e0251
Tyler Osborne <tyler.922@hotmail.com>
2016-02-21 17:50:13 -0500

    Updated SDK

--- rocketBot/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ rocketBot/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -31,10 +31,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import com.qualcomm.ftcrobotcontroller.opmodes.ftcdefault.rocketBot;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
-
 
 /**
  * Register Op Modes
@@ -56,7 +54,47 @@ public class FtcOpModeRegister implements OpModeRegister {
      *
      * If two or more op modes are registered with the same name, the app will display an error.
      */
-    manager.register("Gaia", rocketBot.class);
-    manager.register("test", K9TankDrive.class);
+
+    manager.register("NullOp", NullOp.class);
+
+    //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
+    manager.register("K9TeleOp", K9TeleOp.class);
+    manager.register("K9Line", K9Line.class);
+    manager.register ("PushBotAuto", PushBotAuto.class);
+    manager.register ("PushBotManual", PushBotManual.class);
+
+
+
+    /*
+     * Uncomment any of the following lines if you want to register an op mode.
+     */
+    manager.register("MR Gyro Test", MRGyroTest.class);
+
+    //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
+    //manager.register("ColorSensorDriver", ColorSensorDriver.class);
+
+    //manager.register("IrSeekerOp", IrSeekerOp.class);
+    //manager.register("CompassCalibration", CompassCalibration.class);
+    //manager.register("I2cAddressChangeExample", LinearI2cAddressChange.class);
+
+
+    //manager.register("NxtTeleOp", NxtTeleOp.class);
+
+    //manager.register("LinearK9TeleOp", LinearK9TeleOp.class);
+    //manager.register("LinearIrExample", LinearIrExample.class);
+
+
+    //manager.register ("PushBotManual1", PushBotManual1.class);
+    //manager.register ("PushBotAutoSensors", PushBotAutoSensors.class);
+    //manager.register ("PushBotIrEvent", PushBotIrEvent.class);
+
+    //manager.register ("PushBotManualSensors", PushBotManualSensors.class);
+    //manager.register ("PushBotOdsDetectEvent", PushBotOdsDetectEvent.class);
+    //manager.register ("PushBotOdsFollowEvent", PushBotOdsFollowEvent.class);
+    //manager.register ("PushBotTouchEvent", PushBotTouchEvent.class);
+
+    //manager.register("PushBotDriveTouch", PushBotDriveTouch.java);
+    //manager.register("PushBotIrSeek", PushBotIrSeek.java);
+    //manager.register("PushBotSquare", PushBotSquare.java);
   }
 }
--- rocketBot/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9TankDrive.java
+++ rocketBot/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9TankDrive.java
@@ -33,6 +33,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 /**
@@ -47,12 +48,28 @@ public class K9TankDrive extends OpMode {
 	 * as the arm servo approaches 0, the arm position moves up (away from the floor).
 	 * Also, as the claw servo approaches 0, the claw opens up (drops the game element).
 	 */
+    // TETRIX VALUES.
+    final static double ARM_MIN_RANGE  = 0.20;
+    final static double ARM_MAX_RANGE  = 0.90;
+    final static double CLAW_MIN_RANGE  = 0.20;
+    final static double CLAW_MAX_RANGE  = 0.7;
 
+	// position of the arm servo.
+	double armPosition;
+
+	// amount to change the arm servo position.
+	double armDelta = 0.1;
+
+	// position of the claw servo
+	double clawPosition;
+
+	// amount to change the claw servo position by
+	double clawDelta = 0.1;
 
 	DcMotor motorRight;
 	DcMotor motorLeft;
-
-
+	Servo claw;
+	Servo arm;
 
 	/**
 	 * Constructor
@@ -88,9 +105,12 @@ public class K9TankDrive extends OpMode {
 		motorLeft = hardwareMap.dcMotor.get("motor_1");
 		motorLeft.setDirection(DcMotor.Direction.REVERSE);
 
+		arm = hardwareMap.servo.get("servo_1");
+		claw = hardwareMap.servo.get("servo_6");
 
 		// assign the starting position of the wrist and claw
-
+		armPosition = 0.2;
+		clawPosition = 0.2;
 	}
 
 	/*
@@ -127,7 +147,47 @@ public class K9TankDrive extends OpMode {
 		motorLeft.setPower(left);
 
 		// update the position of the arm.
+		if (gamepad1.a) {
+			// if the A button is pushed on gamepad1, increment the position of
+			// the arm servo.
+			armPosition += armDelta;
+		}
 
+		if (gamepad1.y) {
+			// if the Y button is pushed on gamepad1, decrease the position of
+			// the arm servo.
+			armPosition -= armDelta;
+		}
+
+        // update the position of the claw
+        if (gamepad1.left_bumper) {
+            clawPosition += clawDelta;
+        }
+
+        if (gamepad1.left_trigger > 0.25) {
+            clawPosition -= clawDelta;
+        }
+
+        if (gamepad1.b) {
+            clawPosition -= clawDelta;
+        }
+
+		// update the position of the claw
+		if (gamepad1.x) {
+			clawPosition += clawDelta;
+		}
+
+		if (gamepad1.b) {
+			clawPosition -= clawDelta;
+		}
+
+		// clip the position values so that they never exceed their allowed range.
+		armPosition = Range.clip(armPosition, ARM_MIN_RANGE, ARM_MAX_RANGE);
+		clawPosition = Range.clip(clawPosition, CLAW_MIN_RANGE, CLAW_MAX_RANGE);
+
+		// write position values to the wrist and claw servo
+		arm.setPosition(armPosition);
+		claw.setPosition(clawPosition);
 
 		/*
 		 * Send telemetry data back to driver station. Note that if we are using
@@ -137,7 +197,8 @@ public class K9TankDrive extends OpMode {
 		 */
 
 		telemetry.addData("Text", "*** Robot Data***");
-
+        telemetry.addData("arm", "arm:  " + String.format("%.2f", armPosition));
+        telemetry.addData("claw", "claw:  " + String.format("%.2f", clawPosition));
 		telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", left));
 		telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", right));
 	}
